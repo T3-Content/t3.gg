5729acca354a65227be9d09fdc37dc3195eb01fc
bryanthaboi <bryanthaboi@theboisclub.com>
2026-01-15 15:55:45 -0500

    win95 theme and theme switcher

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import prefetch from "@astrojs/prefetch";
 import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,7 +9,7 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
   plugins: [],
-  integrations: [prefetch()],
+  integrations: [prefetch(), icon()],
   redirects: {
     "/links": "/",
     "/faq": "/",
